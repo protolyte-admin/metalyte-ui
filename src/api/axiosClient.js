813c80e604534a226a355f037526294fa917c9c0
shreeeeee81 <shreeeee81@gmail.com>
2026-08-02 12:49:42 +0530

    Added consolel og

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+console.log("VITE_API_BASE_URL =", import.meta.env.VITE_API_BASE_URL);
 const axiosClient = axios.create({
-    baseURL: "/api/v1",
+    baseURL: import.meta.env.VITE_API_BASE_URL || "/api/v1",
+    
     headers: { "Content-Type": "application/json" }
 });
 
