fc9582e1ca2b5a3fe2505cffd8146905f427bbc3
shreeeeee81 <shreeeee81@gmail.com>
2026-08-06 22:57:28 +0530

    Updated config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
         port: 5173,
         proxy: {
             "/api": {
-                target: "https://whatsapp-engine-mz3d.onrender.com",
+                target: "https://whatsapp-engine-mz3d.onrender.com", 
                 // target: "http://localhost:8080",
                 // target: "https://protolyte-whatsapp-engine-gcedanfxd7azhte4.eastasia-01.azurewebsites.net",
                 // target: "https://sizes-christ-written-usage.trycloudflare.com",
